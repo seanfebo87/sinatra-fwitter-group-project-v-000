1bc50360988deef9345abf16363eef2a7c4fbab2
Sean  Febo <seanfebo@techie.com>
2018-10-17 23:08:05 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 require './config/environment'
 class ApplicationController < Sinatra::Base
-  
+   configure do	  configure do
+    set :public_folder, 'public'	    set :public_folder, 'public'
+    set :views, 'app/views'
+    
   get '/' do 
     erb :index
   end
